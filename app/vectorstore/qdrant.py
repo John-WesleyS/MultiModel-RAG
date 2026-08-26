cbc038285940f6148be145372d55e7330c2ff16a
john <johnwesleybarre588@gmail.com>
2026-08-26 22:59:43 +0530

    added files for testing the query and completed query phase

--- app/vectorstore/qdrant.py
+++ app/vectorstore/qdrant.py
@@ -58,3 +58,24 @@ def store_chunks(chunks):
 
         points=points
     )
+    
+def search_similar_chunks(query_vector, top_k=5):
+
+    results = client.query_points(
+        collection_name=COLLECTION_NAME,
+        query=query_vector,
+        limit=top_k,
+        with_payload=True
+    )
+
+    retrieved_chunks = []
+
+    for result in results.points:
+
+        retrieved_chunks.append({
+            "text": result.payload["text"],
+            "metadata": result.payload["metadata"],
+            "score": result.score
+        })
+
+    return retrieved_chunks         
---
+++ test_retrieval.py
@@ -0,0 +1,30 @@
+from app.embeddings.embedder import generate_query_embedding
+from app.vectorstore.qdrant import search_similar_chunks
+
+
+query = "explain about SOFTWARE MAINTENANCE PROCESS models"
+
+query_vector = generate_query_embedding(query)
+
+results = search_similar_chunks(
+    query_vector,
+    top_k=5
+)
+
+print("\nQuery:")
+print(query)
+
+print("\nRetrieved chunks:")
+
+for i, result in enumerate(results, start=1):
+
+    print("\n-----------------------------")
+
+    print(f"Result {i}")
+
+    print("Score:", result["score"])
+
+    print("Metadata:", result["metadata"])
+
+    print("Text:")
+    print(result["text"])
